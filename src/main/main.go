366e708426e070f63e69f66fd3a827df1f1acc90
samtcifihi <la.lojban@gmail.com>
2021-03-02 09:35:54 -0500

    Base Functionality Done
    Listing entrants and pairing is still manual.

--- src/main/main.go
+++ src/main/main.go
@@ -1,11 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
 	// slice of entrants
 	entrants := []entrant{}
 
 	// List of Entrants
-	addEntrant("test", 1283, 3, &entrants) // 10k
+	addEntrant("test", 1283, 3, &entrants)         // 10k
+	addEntrant("testChinitsu", 1994, 3, &entrants) // 1d
+
+	// List of Matches
+	fmt.Println(matchStr(entrants[0], entrants[1]))
 }
 
 func addEntrant(username string, rating float64, gameRegCount uint8, entrants *[]entrant) {
@@ -16,4 +25,10 @@ func addEntrant(username string, rating float64, gameRegCount uint8, entrants *[
 // pass entrant.internalRank for both parameters
 func rkomi(bRank int, wRank int) int {
 	return 7 + bRank - wRank
+}
+
+func matchStr(black entrant, white entrant) string {
+	return black.username + " [" + black.displayRank + "] vs. " +
+		white.username + " [" + white.displayRank + "] (" +
+		strconv.Itoa(rkomi(black.internalRank, white.internalRank)) + " komi)"
 }
