16d033d65493a4884655e37179945fff539348ba
samtcifihi <la.lojban@gmail.com>
2021-03-02 09:14:29 -0500

    addEntrants()

--- src/main/main.go
+++ src/main/main.go
@@ -1,5 +1,13 @@
 package main
 
 func main() {
+	// slice of entrants
+	entrants := []entrant{}
+	// pass to addEntrant()
+	addEntrant("test", 1283, 3, &entrants) // 10k
+}
 
+func addEntrant(username string, rating float64, gameRegCount uint8, entrants *[]entrant) {
+	e := newEntrant(username, rating, gameRegCount)
+	*entrants = append(*entrants, *e)
 }
